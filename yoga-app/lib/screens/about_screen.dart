c20615c0237e5c54255312937e435d50a91aefe8
yameeshnayak04 <nayak.yameesh@gmail.com>
2025-11-04 00:26:00 +0530

    fix(frontend) : changed user manual to doc and updated it

--- yoga-app/lib/screens/about_screen.dart
+++ yoga-app/lib/screens/about_screen.dart
@@ -87,7 +87,7 @@ class AboutScreen extends StatelessWidget {
                         onPressed: () {
                           // ❗ IMPORTANT: Replace this with the actual URL of your PDF file
                           const pdfUrl =
-                              'https://drive.google.com/file/d/1pRUboC_Uq7P_QSo_O9OY_h4zB05BdyNL/view?usp=drive_link';
+                              'https://docs.google.com/document/d/1svPXe2R8nSMmSGjmI1dy_1CGKGKLNw3qILFhWwC-uX0/edit?usp=sharing';
                           _launchURL(context, pdfUrl);
                         },
                       ),
